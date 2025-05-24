12ee9c7adc7d7181b4cbae3676a7a437eaf96e18
prog-ops <akunbaru14102019@gmail.com>
2025-05-24 13:23:15 +0700

    Add inbox type

---
+++ src/types/inbox.ts
@@ -0,0 +1,16 @@
+export interface InboxItemOwner {
+    id: string;
+    firstName: string;
+    lastName: string;
+    picture: string; // URL avatar
+}
+
+export interface InboxItem {
+    id: string;
+    text: string; // Akan kita gunakan sebagai preview pesan
+    publishDate: string; // Timestamp
+    owner: InboxItemOwner;
+    // Kita akan tambahkan properti ini secara manual untuk UI
+    subject?: string;
+    read: boolean;
+}
