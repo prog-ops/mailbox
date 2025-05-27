248392181c2180f0b56304ccb1bd0452f3a9bada
prog-ops <akunbaru14102019@gmail.com>
2025-05-27 13:21:43 +0700

    Contain App in localization

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import './index.css'
 import App from './App.tsx'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <App/>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <App/>
+            </LocalizationProvider>
         </QueryClientProvider>
     </StrictMode>,
 )
